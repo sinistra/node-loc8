4981507da446247e236e6d75f39b8dd6c15007ba
paul taylor <ptaylor@macquarietelecom.com>
2018-07-31 13:50:21 +1000

    add bootstrap-css-modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import logo from '../images/logo-macquarie-telecom.png';
 import styles from './Header.css';
+// import { pull } from 'bootstrap-css-modules/css/pull.css';
 
 class Header extends Component {
     render() {
         return (
-            <div className={styles.Header}>
+            <div className={`pull-left ${styles.Header}`}>
                 <img src={logo} className={styles.Logo} alt="logo"/>
-                <div className={styles.Title}>Welcome to LOC-8</div>
+                <span className={styles.Title}>Welcome to LOC-8</span>
             </div>
         );
     }
